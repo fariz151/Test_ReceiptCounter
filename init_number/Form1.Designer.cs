16d58b2a5fe1b7b4d47db00faece15a044339e86
Fariz151 <farizsite@yahoo.com>
2022-08-22 10:57:11 +0800

    tambah solution file & installer

---
+++ init_number/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace init_number
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Gold;
+            this.button1.Location = new System.Drawing.Point(53, 144);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(17, 12);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "nombor giliran";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // printDocument1
+            // 
+            this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
+            // 
+            // printPreviewDialog1
+            // 
+            this.printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
+            this.printPreviewDialog1.Enabled = true;
+            this.printPreviewDialog1.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog1.Icon")));
+            this.printPreviewDialog1.Name = "printPreviewDialog1";
+            this.printPreviewDialog1.Visible = false;
+            this.printPreviewDialog1.Load += new System.EventHandler(this.printPreviewDialog1_Load);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "current number";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Branch ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(174, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 24);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "kaunterNo";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(13, 111);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(105, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Prev_Number";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(175, 115);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(87, 16);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Prev_kaunter";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(12, 63);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(103, 24);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "No Giliran";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(174, 71);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(72, 20);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Kaunter";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 168);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+        private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ init_number/Form1.cs
@@ -0,0 +1,783 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.Configuration;
+using System.Drawing.Printing;
+namespace init_number
+{
+    public partial class Form1 : Form
+    {
+
+
+        static string constring = ConfigurationManager.ConnectionStrings["Conns"].ConnectionString;
+        MySqlConnection dbCon = new MySqlConnection(constring);
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            int initial;
+            int num;
+            int result;
+            string nomborstring;
+
+
+            int convertnum;
+            int cmt;
+            int qfinal1;
+            string convqfinal;
+            int inserstRecord;
+            string convtostring;
+            int insertData;
+
+            DataTable ds;
+            DataTable ds1;
+            int resultinsert;
+
+            int qtype = 1;
+
+            ds1 = currentQueue_1();
+
+            if (ds1.Rows.Count > 0)
+            {
+
+
+                ds = currentMaxQueue_1();
+                if (ds.Rows.Count > 0)
+                {
+
+                    int toInteger;
+
+
+                    DataRow drnama = ds.Rows[0];
+                    nomborstring = drnama["currMax"].ToString();
+
+                    toInteger = Convert.ToInt32(nomborstring);
+
+                    toInteger = toInteger + 1;
+                    string conInt;
+                    DateTime time;
+                    time = DateTime.Now;
+
+                    int bilcal = 0;
+                    string fs;
+                    conInt = Convert.ToString(toInteger).ToString();
+                    string t_type = "G";
+                    fs = DateTime.Now.ToString("HH:mm:ss");
+
+
+
+                    insertData = InsertQue(conInt, fs, bilcal, qtype, t_type);
+
+                    printPreviewDialog1.Document = printDocument1;
+                    printDocument1.DefaultPageSettings.PaperSize = new PaperSize("Custom", 300, 500);
+                    printPreviewDialog1.ShowDialog();
+
+                }
+
+                else
+                {
+
+
+
+
+
+                }
+
+
+
+            }
+            else
+            {
+
+
+
+
+
+
+                initial = 1000;
+
+                initial = initial + 1;
+                result = initial;
+
+                DateTime time;
+                string fs;
+                fs = DateTime.Now.ToString("HH:mm:ss");
+                //Dim time As DateTime = DateTime.Now
+                //Dim format As String = " HH:mm:ss"
+                //Dim fs As String
+                int bilcal = 0;
+                string t_type = "G";
+                convtostring = Convert.ToString(result);
+                insertData = InsertQue(convtostring, fs, bilcal, qtype, t_type);
+
+
+                if (insertData > 0)
+                {
+
+
+                    printPreviewDialog1.Document = printDocument1;
+                    printDocument1.DefaultPageSettings.PaperSize = new PaperSize("Custom", 300, 500);
+
+                    printPreviewDialog1.ShowDialog();
+
+
+                }
+
+                else
+                {
+
+
+
+
+
+
+
+
+
+
+
+                }
+
+
+
+            }
+
+
+        
+        }
+
+
+
+
+        public DataTable currentQueue_2()
+        {
+
+            DataTable dt = new DataTable();
+            using (MySqlConnection dbCon = new MySqlConnection(constring))
+            {
+                string strQry = " SELECT  currQue  FROM queue where qtype=2";
+
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = strQry;
+                cmd.Connection = dbCon;
+                dbCon.Open();
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                da.Fill(dt);
+            }
+
+            return dt;
+        }
+
+
+
+        public DataTable currentCalled()
+        {
+            DataTable dt = new DataTable();
+            using (MySqlConnection dbCon = new MySqlConnection(constring))
+            {
+                string strQry = "  SELECT *FROM called_number order by cid desc limit 1 ";
+
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = strQry;
+                cmd.Connection = dbCon;
+                dbCon.Open();
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                da.Fill(dt);
+
+            }
+
+            return dt;
+
+        }
+
+
+
+        public int InsertQue(string number, string timeCounter, int bilcal, int qtype, string t_type)
+        {
+            var result = 0;
+            int tempval = 0;
+            using (MySqlConnection dbCon = new MySqlConnection(constring))
+            {
+                string strQry = "insert into queue(currQue,time_counter,bil_call, qtype,trans_t)values(@number,@timeCounter,@bilcal,@qtype,@t_type)";
+
+
+
+                MySqlCommand cmd = new MySqlCommand();
+                DataTable dt = new DataTable();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = strQry;
+                cmd.Connection = dbCon;
+                dbCon.Open();
+                try
+                {
+                    cmd.Parameters.AddWithValue("@number", number);
+                    cmd.Parameters.AddWithValue("@timeCounter", timeCounter);
+                    cmd.Parameters.AddWithValue("@qtype", qtype);
+                    cmd.Parameters.AddWithValue("@bilcal", bilcal);
+                    cmd.Parameters.AddWithValue("@t_type", t_type);
+                    result = cmd.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+            return result;
+
+
+
+
+        }
+
+
+        public DataTable currentQueue_1()
+        {
+            DataTable dt = new DataTable();
+            using (MySqlConnection dbCon = new MySqlConnection(constring))
+            {
+                string strQry = " SELECT  currQue  FROM queue where qtype=1";
+
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = strQry;
+                cmd.Connection = dbCon;
+                dbCon.Open();
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                da.Fill(dt);
+            }
+            return dt;
+        }
+
+
+
+
+
+
+        public DataTable DisplayCawangan()
+        {
+            DataTable dt = new DataTable();
+            using (MySqlConnection dbCon = new MySqlConnection(constring))
+            {
+                string strQry = "select * from code_caw join cawangan on code_caw.id_caw=cawangan.caw_code ";
+
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = strQry;
+                cmd.Connection = dbCon;
+                dbCon.Open();
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                da.Fill(dt);
+            }
+            return dt;
+
+
+
+        }
+
+
+        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+
+
+
+
+
+
+            DataTable ds;
+            ds = DisplayCawangan();
+
+            string s;
+            string a;
+            string p;
+            if (ds.Rows.Count > 0)
+            {
+                DataRow drnama = ds.Rows[0];
+                s = drnama["nama_cawangan"].ToString();
+                a = drnama["alamat"].ToString();
+                p = drnama["poskod"].ToString();
+
+
+
+                //  e.Graphics.DrawString(s, new Font("Arial", 14, FontStyle.Bold), Brushes.Black, new Point(20, 55));
+
+                e.Graphics.DrawString(s, new Font("Arial", 14, FontStyle.Bold), Brushes.Black, new Point(38, 55));
+
+
+                DataTable ds2;
+                string w;
+                ds2 = FindQueue();
+                if (ds.Rows.Count > 0)
+                {
+
+                    DataRow drnama1 = ds2.Rows[0];
+
+                    w = drnama1["currQue"].ToString();
+
+                    e.Graphics.DrawString("Nombor Giliran Anda :", new Font("Arial", 10, FontStyle.Bold), Brushes.Black, new Point(41, 78));
+                    e.Graphics.DrawString(w, new Font("Arial", 28, FontStyle.Bold), Brushes.Black, new Point(100, 100));
+
+
+
+
+                    DataTable dcalled;
+                    dcalled = FindLatestCalled();
+                    if (dcalled.Rows.Count > 0)
+                    {
+                        DataRow drcalled = dcalled.Rows[0];
+                        string noLatest = drcalled["que_number"].ToString();
+
+
+                        e.Graphics.DrawString("Nombor Giliran Semasa :", new Font("Arial", 10, FontStyle.Regular), Brushes.Black, new Point(40, 148));
+
+                        e.Graphics.DrawString(noLatest, new Font("Arial", 10, FontStyle.Regular), Brushes.Black, new Point(198, 148));
+
+
+                    }
+
+                    else
+                    {
+
+
+                    }
+
+                    e.Graphics.DrawString("SILA DUDUK & ANDA DILAYAN SEBENTAR LAGI", new Font("Arial", 6, FontStyle.Bold), Brushes.Black, new Point(45, 165));
+
+                    DateTime now = DateTime.Now;
+
+                    string hari_ini = now.ToString("yyyy/MM/dd");
+                    e.Graphics.DrawString(hari_ini, new Font("Arial", 7, FontStyle.Bold), Brushes.Black, new Point(25, 189));
+
+
+                    //             //Dim time1 As DateTime = DateTime.Now
+                    //Dim format1 As String = " HH:mm:ss:tt"
+
+                    DateTime time1 = DateTime.Now;
+
+                    DateTime.Now.ToString("h:mm:ss tt");
+
+                    e.Graphics.DrawString(DateTime.Now.ToString("h:mm:ss tt"), new Font("Arial", 7, FontStyle.Bold), Brushes.Black, new Point(198, 189));
+                    // DateTime thisDay = DateTime.Today;
+                    // e.Graphics.DrawString(thisDay.ToString("D"), new Font("Arial", 10, FontStyle.Bold), Brushes.Black, new Point(55, 150));
+
+                    //string words = "SILA DUDUK, ANDA AKAN DILAYAN SEBENTAR LAGI";
+                    //e.Graphics.DrawString(words, new Font("Arial Black",9, FontStyle.Regular), Brushes.Black, new Point(60,180));
+
+                }
+                else
+                {
+
+
+                }
+
+
+
+
+
+
+
+
+
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+        private void printPreviewDialog1_Load(object sender, EventArgs e)
+        {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+        public DataTable FindQueue()
+        {
+            DataTable dt = new DataTable();
+            using (MySqlConnection dbCon = new MySqlConnection(constring))
+            {
+                string strQry = "SELECT * FROM queue order by id desc limit 1";
+
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = strQry;
+                cmd.Connection = dbCon;
+                dbCon.Open();
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                da.Fill(dt);
+            }
+            return dt;
+        }
+
+
+
+        public DataTable FindLatestCalled()
+        {
+            DataTable dt = new DataTable();
+            using (MySqlConnection dbCon = new MySqlConnection(constring))
+            {
+                string strQry = "SELECT * FROM called_number order by cid desc limit 1";
+
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = strQry;
+                cmd.Connection = dbCon;
+                dbCon.Open();
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                da.Fill(dt);
+            }
+            return dt;
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public DataTable DisplayCaw()
+        {
+            DataTable dt = new DataTable();
+            using (MySqlConnection dbCon = new MySqlConnection(constring))
+            {
+
+                string strQry = "select code_caw.nama_cawangan as cwgn from code_caw join cawangan on code_caw.id_caw=cawangan.caw_code where caw_code=1 ";
+
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = strQry;
+                cmd.Connection = dbCon;
+                dbCon.Open();
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                da.Fill(dt);
+                dt.Dispose();
+                da.Dispose();
+                cmd.Dispose();
+                dbCon.Close();
+            }
+            return dt;
+        }
+
+
+
+
+
+
+        public DataTable currentMaxQueue_1()
+        {
+            DataTable dt = new DataTable();
+            using (MySqlConnection dbCon = new MySqlConnection(constring))
+            {
+                string strQry = "SELECT MAX(currQue)as currMax FROM queue where qtype=1";
+
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = strQry;
+                cmd.Connection = dbCon;
+                dbCon.Open();
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                da.Fill(dt);
+            }
+
+            return dt;
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+
+
+            DataTable d4;
+            d4 = DisplayCaw();
+            if (d4.Rows.Count > 0)
+            {
+                DataRow dr = d4.Rows[0];
+
+                string caw = dr["cwgn"].ToString();
+
+                label2.Text = caw;
+            }
+
+            else
+            {
+
+
+            }
+
+
+
+
+            DataTable dtC;
+            string numbergiliran;
+            int qid;
+            string kaunter;
+
+            dtC = currentCalled();
+            DataTable dtprev;
+
+
+            if (dtC.Rows.Count > 0)
+            {
+
+                DataRow dr = dtC.Rows[0];
+                numbergiliran = dr["que_number"].ToString();
+                kaunter = dr["count_number"].ToString();
+                qid = Convert.ToInt16(dr["cid"]);
+
+                label1.Text = numbergiliran;
+                label3.Text = kaunter;
+                DataTable d2;
+                int id;
+                string w;
+                string k;
+                   d2 = PrevQueue(qid);
+                   if (d2.Rows.Count > 0)
+                   {
+
+                       DataRow dr2 = d2.Rows[0];
+
+                       id = Convert.ToInt16(dr2["cid"]);
+                       w = dr2["que_number"].ToString();
+                       k = dr2["count_number"].ToString();
+
+
+                       label4.Text = w;
+                       label5.Text = k;
+
+
+                   }
+
+            }
+
+            else
+            {
+
+
+
+
+
+            }
+
+
+         
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+        public DataTable PrevQueue(int qid)
+        {
+            /*int num = qid;
+
+
+            string strQry = "SELECT * FROM called_number WHERE cid < " + num + " ORDER BY cid DESC LIMIT 1";
+
+            MySqlCommand cmd = new MySqlCommand();
+            DataTable dt = new DataTable();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = strQry;
+            cmd.Connection = dbCon;
+            dbCon.Open();
+            MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+            da.Fill(dt);
+            dt.Dispose();
+            da.Dispose();
+            cmd.Dispose();
+            dbCon.Close();
+            return dt;
+            */
+
+
+            DataTable dt = new DataTable();
+            int num = qid;
+
+            using (MySqlConnection dbCon = new MySqlConnection(constring))
+            {
+                string strQry = "SELECT * FROM called_number WHERE cid < " + num + " ORDER BY cid DESC LIMIT 1";
+
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = strQry;
+                cmd.Connection = dbCon;
+                dbCon.Open();
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                da.Fill(dt);
+
+            }
+
+            return dt;
+
+
+
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.KeyPreview = true;
+            timer1.Start();
+
+        }
+
+
+
+      
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+
+
+
+            switch (e.KeyCode)
+            {
+                case Keys.NumPad7:
+                    e.Handled = true;
+                    button1.PerformClick();
+                    break;
+
+
+
+
+
+            }
+
+
+
+
+
+
+            
+
+
+
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+
+                case Keys.NumPad7:
+                    e.Handled = true;
+                    //button1.PerformClick();
+                    button1.PerformClick();
+                    break;
+
+                // And so on
+
+
+
+
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+   
+       
+    }
+
+}
